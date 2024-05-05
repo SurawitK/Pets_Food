331fc3e61d59a509950acff34a4494ddf4d46d20
possionthefish1 <92135698+possionthefish1@users.noreply.github.com>
2024-05-06 00:09:48 +0700

    remove useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import Footer from "./components/Footer"
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import Footer from './components/Footer';
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <Footer/>
-  );
+	return <Footer />;
 }
 
 export default App;
