e042e13171b74a4db45efe44e1b51631c45bed53
SurawitK <sk.helloworld@gmail.com>
2024-05-10 23:18:03 +0700

    MergeBug

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,16 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
-<<<<<<< HEAD
 import Carousel from './components/Carousel/Carousel';
-=======
 import Footer from './components/Footer/Footer';
->>>>>>> 82802e45b89f08e718964e74f1347a2eb657e950
 
 function App() {
 	return (
 		<>
 			<Navbar />
 			<Hero />
-<<<<<<< HEAD
 			<Carousel />
-=======
 			<Footer />
->>>>>>> 82802e45b89f08e718964e74f1347a2eb657e950
 		</>
 	);
 }
