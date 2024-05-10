0cc6828ddb38abfe9479613c69b880f6816a759a
possionthefish1 <92135698+possionthefish1@users.noreply.github.com>
2024-05-10 20:49:37 +0700

    delete carousels

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,12 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
-import Footer from './components/Footer';
-import Carousel from './components/Carousel';
-import CarouselCss from './components/CarouselCss';
-
 
 function App() {
 	return (
 		<>
 			<Navbar />
 			<Hero />
-			{/* <Carousel/> */}
-			<CarouselCss/>
 		</>
 	);
 }
