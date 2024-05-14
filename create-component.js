41f971f236b50421b39e654821626059a4cf4ee5
possionthefish1 <92135698+possionthefish1@users.noreply.github.com>
2024-05-14 22:12:50 +0700

    ts god

--- create-component.js
+++ create-component.js
@@ -3,24 +3,24 @@ import fs from "fs"; //(this is for typescript change .ts , .tsx to .js .jsx for
 const componentName = `${process.argv[2]}`;
 const componentDirectory = `./src/components/${componentName}`;
 const componentFiles = [
-  {
-    name: `${componentName}.jsx`,
-    content: `import React from 'react';\nimport styles from './${process.argv[2]}.module.css';\n
+	{
+		name: `${componentName}.jsx`,
+		content: `import React from 'react';\nimport styles from './${process.argv[2]}.module.css';\n
 function ${process.argv[2]}() {\n\treturn <div>${process.argv[2]}</div>;\n}\n\nexport default ${process.argv[2]};
 `,
-  },
-  { name: `${componentName}.module.css`, content: "" },
-  {
-    name: `index.jsx`,
-    content: `export { default } from './${componentName}';`,
-  },
+	},
+	{ name: `${componentName}.module.css`, content: "" },
+	{
+		name: "index.jsx",
+		content: `export { default } from './${componentName}';`,
+	},
 ];
 
 if (!fs.existsSync(componentDirectory)) {
-  fs.mkdirSync(componentDirectory);
+	fs.mkdirSync(componentDirectory);
 }
 
 for (const file of componentFiles) {
-  const filePath = `${componentDirectory}/${file.name}`;
-  fs.writeFileSync(filePath, file.content);
+	const filePath = `${componentDirectory}/${file.name}`;
+	fs.writeFileSync(filePath, file.content);
 }
