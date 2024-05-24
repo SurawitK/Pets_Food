8b09c35696d7b83602ceac834cbc7aabf884a984
possionthefish1 <92135698+possionthefish1@users.noreply.github.com>
2024-05-24 23:10:35 +0700

    fix tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+/* biome-ignore lint: <explanation> */
 export default {
-	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-	theme: {
-		fontFamily: {
-			mono: ["Ubuntu Mono", "monospace"],
-			serif: ["Bree Serif", "serif"],
-			lemon: ["Lemon", "serif"],
-		},
-		extend: {},
-	},
-	plugins: [],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    fontFamily: {
+      mono: ['Ubuntu Mono', 'monospace'],
+      serif: ['Bree Serif', 'serif'],
+      lemon: ['Lemon', 'serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
 };
