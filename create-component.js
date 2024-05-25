95d220da2002ab85099c49741a176f6e0a5352ab
possionthefish1 <92135698+possionthefish1@users.noreply.github.com>
2024-05-26 00:33:07 +0700

    godbu

--- create-component.js
+++ create-component.js
@@ -1,7 +1,7 @@
-import * as fs from 'node:fs' //(this is for typescript change .ts , .tsx to .js .jsx for javascript)
+import fs from 'node:fs'; //(this is for typescript change .ts , .tsx to .js .jsx for javascript)
 
-const componentName = `${process.argv[2]}`
-const componentDirectory = `./src/components/${componentName}`
+const componentName = `${process.argv[2]}`;
+const componentDirectory = `./src/components/${componentName}`;
 const componentFiles = [
   {
     name: `${componentName}.tsx`,
@@ -14,13 +14,13 @@ function ${process.argv[2]}() {\n\treturn <div>${process.argv[2]}</div>;\n}\n\ne
     name: 'index.ts',
     content: `export { ${componentName} } from './${componentName}';`,
   },
-]
+];
 
 if (!fs.existsSync(componentDirectory)) {
-  fs.mkdirSync(componentDirectory)
+  fs.mkdirSync(componentDirectory);
 }
 
 for (const file of componentFiles) {
-  const filePath = `${componentDirectory}/${file.name}`
-  fs.writeFileSync(filePath, file.content)
+  const filePath = `${componentDirectory}/${file.name}`;
+  fs.writeFileSync(filePath, file.content);
 }
